260acaec361ac5ecfed16084e8c52b4279e231ee
rchiarino <rz.chiarino@gmail.com>
2024-02-26 21:26:29 -0300

    best profit

---
+++ easy/bestTimeToBuyAndSell.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let max = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    } else {
+      max = Math.max(max, prices[i] - min);
+    }
+  }
+  return max;
+};
